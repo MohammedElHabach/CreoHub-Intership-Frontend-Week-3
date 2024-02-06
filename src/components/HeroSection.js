830a5afad225d4c9ac3712a137fe58cbd72a6c74
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-02-06 14:04:03 +0200

    fixing nav and banner

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,14 +6,14 @@ import SecurityIcon from '@mui/icons-material/Security';
 const HeroSection = () => {
   
   return (
-    <section id='hero' className='md:bg-grayBg ' >
-        <div className='xl:container px-5 mx-auto py-12  '>
-          <div className='flex flex-col   md:items-start items-center  md:flex-row md:gap-x-4'>
-            <div className='space-y-8'>
+    <section id='hero' className='' >
+        <div className='xl:container px-5 mx-auto py-12 md:py-20 md:pb-56 '>
+          <div className='flex flex-col md:w-3/5    md:items-start items-center  md:flex-row md:gap-x-4'>
+            <div className='space-y-8  '>
               <h1 className='text-4xl font-bold text-center md:text-left '>Search, Find, & Apply</h1>
-              <p className='text-center  md:text-justify md:w-1/2 md:text-lg     '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente atque magni nisi molestias dolor soluta vitae labore, animi officiis, dolore beatae quibusdam quisquam similique ipsam consequuntur, repellat sunt culpa exercitationem.</p>
+              <p className='text-center  md:text-justify md:text-lg  md:pr-10    '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente atque magni nisi molestias dolor soluta vitae labore, animi officiis, dolore beatae quibusdam quisquam similique ipsam consequuntur, repellat sunt culpa exercitationem.</p>
 
-              <div className='  flex flex-wrap gap-y-6 justify-center items-center space-x-4 md:bg-white md:p-5 md:rounded-md  md:justify-start md:w-3/4  '>
+              <div className='  flex flex-wrap gap-y-6 justify-center items-center space-x-4 md:bg-white md:p-5  md:rounded-md  md:justify-start    '>
                   <input placeholder="&#128269;  Job title or keyword" type="text" className=' bg-grayBg p-2 w-56 rounded-md border focus:outline-black  placeholder:text-black' />
                   <select className=' bg-grayBg p-2 w-40 rounded-md border focus:outline-black '>
                       <option disabled selected hidden value="">Location</option>
@@ -21,16 +21,20 @@ const HeroSection = () => {
                   <button className='px-6 py-2 rounded-md font-bold bg-greenBg text-white'>Search</button>
               </div>
             </div>
-
-            <div  className='bg-greenBg w-full py-52 hidden md:flex rounded-bl-3xl'></div>
           </div>
             
         </div>
         <div className='bg-greenBg mt-12 py-52 md:hidden'>
-            <SearchIcon sx={{color:"white",fontSize:"5rem",position:"absolute",top:"45rem",left:"10%"}}/>
-            <SignalCellularAltIcon sx={{color:"white",fontSize:"5rem",position:"absolute",top:"54rem",left:"7%",transform: 'rotate(5deg)'}}/>
-            <SecurityIcon sx={{color:"white",fontSize:"5rem",position:"absolute",top:"48rem",left:"70%",transform: 'rotate(5deg)'}}/> <br /> 
-          </div>
+            <SearchIcon sx={{color:"white",fontSize:"5rem",position:"absolute",top:"39rem",left:"10%"}}/>
+            <SignalCellularAltIcon sx={{color:"white",fontSize:"5rem",position:"absolute",top:"50rem",left:"7%",transform: 'rotate(5deg)'}}/>
+            <SecurityIcon sx={{color:"white",fontSize:"5rem",position:"absolute",top:"40rem",left:"70%",transform: 'rotate(5deg)'}}/> <br /> 
+        </div>
+
+        <div className='hidden md:block'>
+            <SearchIcon sx={{color:"white",fontSize:"5rem",position:"absolute",top:"15%",left:"70%"}}/>
+            <SignalCellularAltIcon sx={{color:"white",fontSize:"5rem",position:"absolute",top:"27%",left:"90%",transform: 'rotate(5deg)'}}/>
+            <SecurityIcon sx={{color:"white",fontSize:"5rem",position:"absolute",top:"35%",left:"67%",transform: 'rotate(5deg)'}}/> <br /> 
+        </div>
 
     </section>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,30 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav id='navbar' className=' bg-grayBg'>
+    <nav id='navbar' className=''>
         <div className="xl:container relative mx-auto p-5">
-            <div className='flex items-center justify-between'>
-                <div className=' mx-auto text-3xl font-bold md:mx-0'>
-                    <Link href="/"  >
-                            <h1 className='flex items-center'>Job <span className=' text-greenBg'>Now</span></h1> 
-                     </Link>
+            <div className='flex items-center justify-between  '>
+                <div className='flex items-center   md:gap-x-8 lg:gap-x-16  space-x-5 lg:space-x-9 md:w-3/5 '>
+                    <div className='mx-auto block text-center  text-3xl font-bold md:mx-0'>
+                        <Link href="/" className=''  >
+                                <h1 className='flex items-center'>Job <span className=' text-greenBg'>Now</span></h1> 
+                        </Link>
+                    </div>
+
+                    <div className='hidden md:flex space-x-5 lg:space-x-9 md:mx-auto'>
+                        <Link className='' href="/">Home</Link>
+                        <Link href="/">Job</Link>
+                        <Link href="/">About Us</Link>
+                        <Link href="/">Contact</Link>
+                    </div>
                 </div>
 
-                <div className='hidden md:flex space-x-6'>
-                    <Link href="/">Home</Link>
-                    <Link href="/">Job</Link>
-                    <Link href="/">About Us</Link>
-                    <Link href="/">Contact</Link>
-                </div>
 
-                <div className='hidden md:flex space-x-3 '>
-                    <button className='text-white'>Sign in</button>
-                    <button className='text-black bg-white px-6 py-2 rounded-md'>Create Account</button>
+                <div className='flex w-2/5 justify-end'>
+                    <div className='hidden md:flex space-x-3 '>
+                        <button className='text-white'>Sign in</button>
+                        <button className='text-black bg-white px-6 py-2 rounded-md'>Create Account</button>
+                    </div>
                 </div>
 
                 <button id="menu-btn" className="block hamburger  md:hidden focus:outline-none">
