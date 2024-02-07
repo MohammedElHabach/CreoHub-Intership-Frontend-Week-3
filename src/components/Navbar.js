f451590f29819a33b6b422a8a00d829067316563
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-02-07 13:18:58 +0200

    fixing hero section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav id='navbar' className=''>
-        <div className="xl:container relative mx-auto p-5 px-12">
+        <div className="xl:container relative mx-auto  px-12">
             <div className='flex items-center justify-between  '>
-                <div className='w-full flex items-center   md:gap-x-8 lg:gap-x-16  space-x-5 lg:space-x-9 md:w-3/5 '>
+                <div className='w-full p-5 flex items-center   md:gap-x-3 lg:gap-x-11  space-x-5 lg:space-x-9 md:w-3/5 '>
                     <div className='mx-auto  text-3xl font-bold md:mx-0'>
                         <Link href="/" className=''  >
                                 <h1 className='flex items-center'>Job <span className=' text-greenBg'>Now</span></h1> 
@@ -22,7 +22,7 @@ const Navbar = () => {
                 </div>
 
 
-                <div className='hidden md:flex w-2/5 justify-end'>
+                <div className='hidden p-5 bg-greenBg md:flex w-2/5 justify-end'>
                     <div className='space-x-3 '>
                         <button className='text-white'>Sign in</button>
                         <button className='text-black bg-white px-6 py-2 rounded-md'>Create Account</button>
