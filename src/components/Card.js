d0a80d76dbd83e09e40b2c1d0e0a9a223e96183e
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-02-08 18:23:10 +0200

    fixing hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Card = ({ icon, title, description, margin }) => {
-  
-
   return (
     <div className='group'>
-      <div className={`p-6 cardCategory transition-all duration-300 bg-white text-black group-hover:bg-greenBg group-hover:text-white rounded-md shadow-md cursor-default mt-5 `}>
-        <div className={`text-3xl text-greenBg transition-colors   mb-4  group-hover:text-white `} >
+      <div className={`p-6 cardCategory bg-white text-black rounded-md shadow-md cursor-default mt-5 
+        transition-all duration-500 group-hover:bg-greenBg group-hover:text-white`}>
+        <div className={`text-3xl text-greenBg mb-4 transition-colors group-hover:text-white`}>
           {icon}
         </div>
-        <h3 className={`text-xl font-bold mb-2  `}>{title}</h3>
-        <p className={` text-gray-600 group-hover:text-white text-justify ${ margin ? "pt-8" : ""}`}>
+        <h3 className={`text-xl text-black font-bold mb-2 transition-colors group-hover:text-white`}>{title}</h3>  
+        <p className={`text-gray-600 text-justify ${margin ? "pt-8" : ""} 
+          transition-colors group-hover:text-white`}>
           {description}
         </p>
       </div>
