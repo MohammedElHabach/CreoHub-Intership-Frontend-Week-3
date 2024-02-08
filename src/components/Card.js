dae4d623d3e329e95236367db158a70b51d9aa46
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-02-08 18:16:13 +0200

    fixing hover

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,19 @@
-"use client";
 import React from 'react';
 
-import { useState } from "react";
-
 const Card = ({ icon, title, description, margin }) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+  
 
   return (
-    <div className={` p-6 cardCategory rounded-md shadow-md cursor-default mt-5 transition-all duration-300 ${isHovered ? 'bg-greenBg text-white':'bg-white text-black'}`}
-      onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
-      <div className={`text-3xl mb-4 transition-colors ${isHovered ? 'text-white':'text-greenBg'}`} >
-        {icon}
+    <div className='group'>
+      <div className={`p-6 cardCategory transition-all duration-300 bg-white text-black group-hover:bg-greenBg group-hover:text-white rounded-md shadow-md cursor-default mt-5 `}>
+        <div className={`text-3xl text-greenBg transition-colors   mb-4  group-hover:text-white `} >
+          {icon}
+        </div>
+        <h3 className={`text-xl font-bold mb-2  `}>{title}</h3>
+        <p className={` text-gray-600 group-hover:text-white text-justify ${ margin ? "pt-8" : ""}`}>
+          {description}
+        </p>
       </div>
-      <h3 className={`text-xl font-bold mb-2`}>{title}</h3>
-      <p
-        className={`${isHovered ? "white" : "text-gray-600"} text-justify ${
-          margin ? "pt-8" : ""
-        }`}
-      >
-        {description}
-      </p>
     </div>
   );
 };
