7875e3fefd44eb9dd2811465a0fa1a7e7b580922
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-02-13 14:32:14 +0200

    fixing bugs

--- src/components/Footer2.js
+++ src/components/Footer2.js
@@ -9,16 +9,17 @@ const Footer2 = () => {
   return (
     <section id="footer" className=" bg-greenBg py-12  ">
       <div className="xl:container mx-auto px-5 mt-32 ">
-        <div className="grid grid-cols-3 gap-x-5 md:hidden ">
+        <div className="grid grid-cols-2 gap-x-7 gap-y-10 md:hidden ">
             <div className='col-span-2 '>
-                <div className='md:order-1'>
+                <div className=''>
                     <Link href="/">
                     <h2 className='  pb-3 font-bold text-3xl  text-black'>Job<span className='text-white '>Now</span></h2>
                     </Link>
                     <p className='text-white '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis commodi, aspernatur itaque numquam ipsam in eos.</p>
                 </div>
+            </div>
 
-                <div className=' cursor-default text-white my-8 md:my-0 md:order-2'>
+                <div className=' cursor-default text-white   '>
                         <h2 className='text-3xl pb-3  font-bold '>Company</h2>
                         <p>Address : 123 Fifth Avenue,New York - 1060,USA</p>
                         <p>Call Us : +(1600) 456 7890</p>
@@ -26,7 +27,36 @@ const Footer2 = () => {
                         <p>Mon Sat : 9:00 AM - 19:00 PM</p>
                 </div>
 
-                <div className='  text-white md:order-4'>
+                <div className='my-auto space-y-3 text-white md:hidden '>
+                    <h2 className='font-bold text-3xl pb-3 '>Pages</h2>
+                    <Link href="/" className='flex '>
+                        <KeyboardArrowRightIcon/>
+                        Home
+                    </Link>
+                    <Link href="/" className='flex '>
+                    <KeyboardArrowRightIcon/>
+                        Booking
+                    </Link>
+                    <Link href="/" className='flex '>
+                    <KeyboardArrowRightIcon/>
+                        Facilities
+                    </Link>
+                    <Link href="/" className='flex '>
+                    <KeyboardArrowRightIcon/>
+                        About Us
+                    </Link>
+                    <Link href="/" className='flex '>
+                    <KeyboardArrowRightIcon/>
+                        Location
+                    </Link>
+                    <Link href="/" className='flex '>
+                    <KeyboardArrowRightIcon/>
+                        Contact
+                    </Link>
+                </div>
+
+
+                <div className='  text-white '>
                         <h2 className='pb-3 text-3xl font-bold'>Contact Us</h2>
                         <div className='flex gap-x-4'>
                             <Link href="/">
@@ -45,65 +75,6 @@ const Footer2 = () => {
                             </Link>
                         </div>
                 </div>
-
-                <div className='hidden md:block  space-y-3 text-white md:order-3 '>
-                    <h2 className='font-bold text-3xl '>Pages</h2>
-                    <Link href="/" className='flex '>
-                        <KeyboardArrowRightIcon/>
-                        Home
-                    </Link>
-                    <Link href="/" className='flex '>
-                    <KeyboardArrowRightIcon/>
-                        Booking
-                    </Link>
-                    <Link href="/" className='flex '>
-                    <KeyboardArrowRightIcon/>
-                        Facilities
-                    </Link>
-                    <Link href="/" className='flex '>
-                    <KeyboardArrowRightIcon/>
-                        About Us
-                    </Link>
-                    <Link href="/" className='flex '>
-                    <KeyboardArrowRightIcon/>
-                        Location
-                    </Link>
-                    <Link href="/" className='flex '>
-                    <KeyboardArrowRightIcon/>
-                        Contact
-                    </Link>
-                </div>
-            </div>
-
-            {/* right div */}
-            <div className=' pagesFooter space-y-3 text-white md:hidden '>
-                    <h2 className='font-bold text-3xl '>Pages</h2>
-                    <Link href="/" className='flex '>
-                        <KeyboardArrowRightIcon/>
-                        Home
-                    </Link>
-                    <Link href="/" className='flex '>
-                    <KeyboardArrowRightIcon/>
-                        Booking
-                    </Link>
-                    <Link href="/" className='flex '>
-                    <KeyboardArrowRightIcon/>
-                        Facilities
-                    </Link>
-                    <Link href="/" className='flex '>
-                    <KeyboardArrowRightIcon/>
-                        About Us
-                    </Link>
-                    <Link href="/" className='flex '>
-                    <KeyboardArrowRightIcon/>
-                        Location
-                    </Link>
-                    <Link href="/" className='flex '>
-                    <KeyboardArrowRightIcon/>
-                        Contact
-                    </Link>
-                </div>
-
         </div>
 
         <div className='hidden md:grid md:grid-cols-5  md:gap-x-6'>
