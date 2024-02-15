6f4e349528f41e09c8bc3d10137d687461dd25d3
Mohammed El Habach <mhamadhabach06@gmail.com>
2024-02-15 13:11:15 +0200

    fixing nav and feedback

--- src/components/FeedBack.js
+++ src/components/FeedBack.js
@@ -7,11 +7,11 @@ const FeedBack = () => {
       <div className="xl:container mx-auto px-12 mt-32">
         <div className=" grid grid-cols-1 md:grid-cols-2 gap-x-4 ">
           <div>
-            <h2 className="text-3xl font-bold text-center order-1 md:text-left">
+            <h2 className="text-3xl font-bold text-center  md:text-left">
               People's Feedback about JobNow!
             </h2>
 
-            <div className=" py-6 text-gray-600 text-center  order-2 md:text-left">
+            <div className=" py-6 text-gray-600 text-center   md:text-left">
               <p className="lg:pr-10 2xl:pr-28">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed,
                 officia maiores? Voluptates beatae ipsam tenetur cupiditate
@@ -20,7 +20,7 @@ const FeedBack = () => {
               </p>
             </div>
 
-            <div className=" py-6 font-bold  text-center order-3 md:text-left">
+            <div className=" py-6 font-bold  text-center  md:text-left">
               <p className="lg:pr-10 2xl:pr-28">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed,
                 officia maiores? Voluptates beatae ipsam tenetur cupiditate
@@ -29,7 +29,7 @@ const FeedBack = () => {
               </p>
             </div>
 
-            <div className="text-center pt-8 order-5 md:text-left">
+            <div className="hidden text-center pt-8  md:text-left">
               <h3 className="font-bold text-xl ">Brown Garcia</h3>
               <div className="pt-3">
                 <p className="text-gray-600 ">
@@ -46,10 +46,29 @@ const FeedBack = () => {
                 </div>
               </div>
             </div>
+            
           </div>
-          <div className="pt-12 order-4">
+          <div className="pt-12 ">
             <img src="/job.jpg" className="rounded-md" alt="image feedback" />
           </div>
+
+          <div className="text-center pt-8  md:text-left">
+              <h3 className="font-bold text-xl ">Brown Garcia</h3>
+              <div className="pt-3">
+                <p className="text-gray-600 ">
+                  Full Stack Developer in XReact Tech
+                </p>
+              </div>
+
+              <div className="flex gap-x-5 justify-center md:justify-start ">
+                <div className="w-9 h-9 mt-3 bg-gray-300  rounded-full  flex items-center justify-center">
+                  <KeyboardBackspaceIcon sx={{ color: "white" }} />
+                </div>
+                <div className="w-9 h-9 mt-3 bg-greenBg  rounded-full  flex items-center justify-center">
+                  <ArrowForwardIcon sx={{ color: "white" }} />
+                </div>
+              </div>
+            </div>
         </div>
       </div>
     </section>
